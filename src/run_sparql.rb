ddce52ddda0ab348f32187e0d459a6c5cc4da03b
dev <dev.aravind@culturecreates.com>
2025-06-23 16:08:47 +0530

    fix(http): replaced http with https

--- src/run_sparql.rb
+++ src/run_sparql.rb
@@ -6,13 +6,13 @@ graph = RDF::Graph.load("ontorefine-output.ttl")
 file = File.read("capacoa-infer-presenter-type.sparql")
 graph.query(SPARQL.parse(file, update: true))
 prefixes = {
-  schema:   RDF::Vocabulary.new("http://schema.org/"),
-  skos:     RDF::Vocabulary.new("http://www.w3.org/2004/02/skos/core#"),
-  capacoa:  RDF::Vocabulary.new("http://capacoa.ca/vocabulary#"),
-  member:  RDF::Vocabulary.new("http://capacoa.ca/member/"),
-  wikidata_property: RDF::Vocabulary.new("http://www.wikidata.org/prop/direct"),
-  wikidata_entity: RDF::Vocabulary.new("http://www.wikidata.org/entity/"),
-  ebu_core: RDF::Vocabulary.new("http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#"),
+  schema:   RDF::Vocabulary.new("https://schema.org/"),
+  skos:     RDF::Vocabulary.new("https://www.w3.org/2004/02/skos/core#"),
+  capacoa:  RDF::Vocabulary.new("https://capacoa.ca/vocabulary#"),
+  member:  RDF::Vocabulary.new("https://capacoa.ca/member/"),
+  wikidata_property: RDF::Vocabulary.new("https://www.wikidata.org/prop/direct"),
+  wikidata_entity: RDF::Vocabulary.new("https://www.wikidata.org/entity/"),
+  ebu_core: RDF::Vocabulary.new("https://www.ebu.ch/metadata/ontologies/ebucore/ebucore#"),
 }
 output_dir = "output"
 Dir.mkdir(output_dir) unless Dir.exist?(output_dir)
